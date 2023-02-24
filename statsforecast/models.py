24c15091a57660be19a2a5f7371810cbeb18a8ac
AndreiTulbure <31302923+tulbureandreit@users.noreply.github.com>
2023-02-24 12:01:28 +0200

    Update models.py

--- statsforecast/models.py
+++ statsforecast/models.py
@@ -887,23 +887,27 @@ class AutoCES(_TS):
         forecasts : dict
             Dictionary with entries `mean` for point predictions and `level_*` for probabilistic predictions.
         """
-        mod = auto_ces(y, m=self.season_length, model=self.model)
-        fcst = forecast_ces(mod, h, level=level)
-        keys = ["mean"]
-        if fitted:
-            keys.append("fitted")
-        res = {key: fcst[key] for key in keys}
-        if level is not None:
-            level = sorted(level)
-            res = {
+        try:
+           mod = auto_ces(y, m=self.season_length, model=self.model)
+           fcst = forecast_ces(mod, h, level=level)
+           keys = ["mean"]
+           if fitted:
+               keys.append("fitted")
+           res = {key: fcst[key] for key in keys}
+           if level is not None:
+               level = sorted(level)
+               res = {
                 **res,
                 **{f"lo-{l}": fcst[f"lo-{l}"] for l in reversed(level)},
                 **{f"hi-{l}": fcst[f"hi-{l}"] for l in level},
-            }
-            if fitted:
+                    }
+               if fitted:
                 # add prediction intervals for fitted values
-                se = _calculate_sigma(y - mod["fitted"], len(y))
-                res = _add_fitted_pi(res=res, se=se, level=level)
+                   se = _calculate_sigma(y - mod["fitted"], len(y))
+                   res = _add_fitted_pi(res=res, se=se, level=level)
+        except Exception("No AutoCES can be fitted!") as ex:
+           print(ex)
+           res = None
         return res
 
     def forward(
